07f7611f4575266da63d148553f16622c27d876d
vosslerbr <vosslerbrady@gmail.com>
2019-11-20 18:42:42 -0600

    Added animated arrows to menu section dropdowns

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ function showHide(id) {
   // set sectionName to passed in string + '-content'
   // to make 'arg-content'
   let sectionName = id + '-content';
+  let sectionArrow = id + '-arrow';
 
   // set sectionToShow to represent the 'arg-content' div that
   // corresponds to section title clicked
   let sectionToShow = document.getElementById(sectionName);
+  let arrowToRotate = document.getElementById(sectionArrow);
 
   // simple if statement to display or hide 'arg-content'
   if (sectionToShow.style.maxHeight === '1000px') {
     sectionToShow.style.maxHeight = '0';
+    arrowToRotate.style.transform = 'rotate(360deg)';
   } else {
     sectionToShow.style.maxHeight = '1000px';
+    arrowToRotate.style.transform = 'rotate(180deg) translateX(10px) translateY(-3px)';
   }
 }
